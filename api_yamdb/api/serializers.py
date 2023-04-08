cdd85cb186a316fc1e2ff777ca1ceb443828ecde
Vladislav Pavelkov <pavelkovvv@yandex.ru>
2023-04-08 17:11:43 +0300

    Fix to PEP8

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers, status
-
 from django.core.validators import RegexValidator
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import status, viewsets, filters
-
 from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
