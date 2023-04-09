d17eafe1e344da0accf355ffc8a8b8162a290308
Николай Драй <ipkissstan@yandex.ru>
2023-04-09 11:18:05 +0300

    fixed, end

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -6,8 +6,8 @@ from titles.models import Title
 class TitleFilter(FilterSet):
     """Фильтр полей произведения."""
     name = CharFilter(field_name='name', lookup_expr='icontains')
-    category = CharFilter(field_name='category__name', lookup_expr='icontains')
-    genre = CharFilter(field_name='genre__name', lookup_expr='icontains')
+    category = CharFilter(field_name='category__slug', lookup_expr='icontains')
+    genre = CharFilter(field_name='genre__slug', lookup_expr='icontains')
 
     class Meta:
         model = Title
